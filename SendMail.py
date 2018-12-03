fded8e8f91d496a0f73c566c031c9b167a8e23aa
faresdev7 <faresdev7@outlook.com>
2018-12-03 20:22:18 +0100

    Update SendMail.py

--- SendMail.py
+++ SendMail.py
@@ -2,18 +2,22 @@ import smtplib
 import sys
 import getpass
 import re    
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
 from email.utils import COMMASPACE, formatdate
 from pathlib import Path
-mypath = Path().absolute()
-mypath=mypath,'/content.txt'
-print(mypath);
-contents = Path('content.txt').read_text()
+
+path = os.path.abspath( __file__ )
+mypath=input("Content path  ")
+print(mypath)
+Emailpath= input("Email path  ")
+print(Emailpath)
+contents = Path(mypath).read_text()
 part2 = MIMEText(contents, 'html')
-with open("email.txt", "r") as fd:
+with open(Emailpath, "r") as fd:
     lines = fd.read().splitlines()
 email = input("email:")
 password = getpass.getpass("Enter your password: ")
@@ -23,11 +27,11 @@ File = input("File:")
 smtp = smtplib.SMTP('smtp.office365.com')
 smtp.connect('smtp.office365.com','587')
 smtp.starttls()
+smtp.login(email, password)
 part = MIMEBase('application', "octet-stream")
 part.set_payload(open(File, "rb").read())
 encoders.encode_base64(part)
 encoders.encode_base64(part2)
-smtp.login(email, password)
 part.add_header('Content-Disposition', 'attachment; filename=attachment.html')
 msg = MIMEMultipart()
 msg['Subject'] = Subject
@@ -35,6 +39,5 @@ msg['From'] = email
 msg.attach(part)
 msg.attach(part2);
 for item in lines:
-	smtp.login(email,password)
     msg['To'] = item
     smtp.sendmail(email, item, msg.as_string())
